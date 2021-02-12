835b7a72b69ba7c63229a65a2b31c2667593500d
Davide Santangelo <davide.santangelo@gmail.com>
2021-02-12 16:04:29 +0100

    Add Feed search entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,8 +1,8 @@
 class EntriesController < ApplicationController
+  before_action :set_timing, only: [:search]
+
   def index
     @pagy, @entries = pagy Entry.latest
-    @timing = Benchmark.measure { Entry.latest }
-
     respond_to do |format|
       format.html
       format.csv { send_data @entries.limit(10_000).to_csv, filename: "latest-entries-#{Date.today}.csv" }
@@ -10,18 +10,27 @@ class EntriesController < ApplicationController
   end
 
   def search
-    @pagy, @entries = pagy total_entries
+    @pagy, @entries = pagy search_entries
 
     respond_to do |format|
       format.html
-      format.csv { send_data total_entries.limit(10_000).to_csv, filename: "entries-#{Date.today}.csv" }
+      format.csv { send_data search_entries.limit(10_000).to_csv, filename: "entries-#{Date.today}.csv" }
     end
   end
 
   private
 
-  def total_entries
-    @timing = Benchmark.measure { Entry.search(params[:q]) }
-    @total_entries ||= Entry.search(params[:q])
+  def search_entries
+    @search_entries ||= Entry.search(params[:q])
+  end
+
+  def set_timing
+    @timing ||= begin
+      if params[:q].present?
+        Benchmark.measure { Entry.search(params[:q]) }
+      else
+        Benchmark.measure { Entry.latest }
+      end
+    end
   end
 end
--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,5 +1,6 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: [:entries]
+  before_action :set_timing, only: [:entries]
 
   def index
     @pagy, @feeds = pagy Feed.all
@@ -13,10 +14,10 @@ class FeedsController < ApplicationController
   def entries
     respond_to do |format|
       format.html do
-        @pagy, @entries = pagy @feed.entries
+        @pagy, @entries = pagy feed_entries
       end
       format.csv do
-        send_data @feed.entries.limit(10_000).to_csv, filename: "feed-#{@feed.title}-entries-#{Date.today}.csv"
+        send_data feed_entries.limit(10_000).to_csv, filename: "feed-#{@feed.title}-entries-#{Date.today}.csv"
       end
     end
   end
@@ -25,5 +26,19 @@ class FeedsController < ApplicationController
 
   def set_feed
     @feed = Feed.find(params[:id])
+  end
+
+  def feed_entries
+    @feed_entries ||= begin
+      if params[:q].present?
+        Entry.where(feed_id: @feed.id).search(params[:q])
+      else
+        Entry.where(feed_id: @feed.id)
+      end
+    end
+  end
+
+  def set_timing
+    @timing = Benchmark.measure { Entry.where(feed_id: @feed.id).search(params[:q]) }
   end
 end
