318b23e8bee16518522e58b07aa335787f1e9c6c
Davide Santangelo <davide.santangelo@gmail.com>
2021-04-19 17:15:48 +0200

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.15.0'
+lock '~> 3.16.0'
 
 set :application, 'datorss'
 set :repo_url, 'git@github.com:davidesantangelo/dato.rss.git'
