21dc96aab64fca09ad2321354f1d0aae78f415da
Davide Santangelo <davide.santangelo@gmail.com>
2022-05-18 16:11:41 +0200

    Update gems to latest version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.16.0'
+lock '~> 3.17.0'
 
 set :application, 'datorss'
 set :repo_url, 'git@github.com:davidesantangelo/dato.rss.git'
